ec591ec33cd51dfdc77a61442a343462b751e4aa
David Jamieson <92800471+DavidJmsn@users.noreply.github.com>
2025-06-17 19:28:44 +0200

    docs: document API quota headers

--- tests/testthat/test-api-client.R
+++ tests/testthat/test-api-client.R
@@ -21,4 +21,21 @@ test_that('api_get uses caching', {
   expect_equal(length(request_registry()$request_signatures$hash), 1)
 })
 
+test_that('APIClient$get attaches response headers', {
+  stub_request('get', 'https://api-web.nhle.com/header') %>%
+    to_return(body = '{"ok":true}', status = 200,
+              headers = list(
+                'X-Requests-Remaining' = '5',
+                'X-Requests-Used' = '10',
+                'X-Requests-Last' = '1'))
+
+  client <- create_nhl_api_client(use_cache = FALSE)
+  res <- client$get('/header')
+
+  hdr <- attr(res, 'response_headers')
+  expect_equal(hdr[['x-requests-remaining']], '5')
+  expect_equal(hdr[['x-requests-used']], '10')
+  expect_equal(hdr[['x-requests-last']], '1')
+})
+
 webmockr::disable()
